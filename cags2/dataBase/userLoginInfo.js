dceb0cce96da96e60e951af4bbee4b776730add3
Seon Kim <jeongahkim8174@gmail.com>
2022-10-02 22:40:57 -0700

    finished fixing errors for DB trying to fix errors for passport

---
+++ cags2/dataBase/userLoginInfo.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    Username:'String',
+    Password:'String'
+})
+const user = mongoose.model("userInfo",userSchema);
+async function run(username,password){
+    const wat = await user.create({Username: username, Password: password});
+}
+//exports database
+module.exports = user;
+//exports function that signs up the user
+module.exports.createAccount = run;
